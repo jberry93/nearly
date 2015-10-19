5cdea6b67197d183bb8ea8074d8b0cdd6708cfe1
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-10-19 11:06:55 -0700

    Resolved #6 added mocha and tested the node server connection on '/'

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require("gulp"),
     nodemon = require("gulp-nodemon"),
+    mocha = require("gulp-mocha"),
     wiredep = require("wiredep").stream;
 
 gulp.task("bower", function() {
   gulp.src("./views/*.html")
     .pipe(wiredep())
     .pipe(gulp.dest("./views/"));
+});
+
+gulp.task("test", function() {
+  return gulp.src("test.js").pipe(mocha());
 });
 
 gulp.task("nodemon", function() {
