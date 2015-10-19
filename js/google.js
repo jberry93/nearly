ec7b5100f776cae94e41b44990343b4e88d319c3
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-10-19 16:04:26 -0700

    #2 added function to remove old markers so that new markers can populate the map

--- js/google.js
+++ js/google.js
@@ -5,6 +5,21 @@ function createMap() {
   var mapOptions = {center: mapCenter, zoom: 14};
   var map = new google.maps.Map(mapContainer, mapOptions);
 
+  // add reference to button
+  var goButton = document.getElementById("go");
+  goButton.addEventListener("click", function() {
+    removeMarkers(); // remove old markers first
+    codeAddress(); // populate map with new markers
+  }, false);
+
+  // remove old markers
+  function removeMarkers() {
+    markerArray.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markerArray = [];
+  }
+
   // add Geocoder service
   var geoCoder = new google.maps.Geocoder();
   function codeAddress() {
@@ -29,27 +44,33 @@ function createMap() {
         if(statusCode === google.maps.places.PlacesServiceStatus.OK) {
           for(var result = 0; result < listOfResults.length; result++) {
             makeMarker(listOfResults[result]);
-            console.log(listOfResults[result]);
           }
+          console.log(markerArray);
         }
       }
     });
   }
 
-  // add reference to button
-  var goButton = document.getElementById("go");
-  goButton.addEventListener("click", codeAddress, false);
-
   // add info windows to markers
   var infoWindow = new google.maps.InfoWindow();
+  var markerArray = [];
   function makeMarker(place) {
     var locationLocation = place.geometry.location;
     var markerOptions = {map: map, position: locationLocation};
     var marker = new google.maps.Marker(markerOptions);
+    markerArray.push(marker);
     google.maps.event.addListener(marker, "click", function() {
       infoWindow.setContent(place.name);
       infoWindow.open(map, this);
     });
+  }
+
+  // get user's location
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showLocation);
+  } else {
+    alert("You need to enable geolocation!");
+    location.reload(true);
   }
 
   // record users' location
@@ -67,12 +88,4 @@ function createMap() {
   );
   var yInput = document.getElementById("y");
   var theSearchBox = new google.maps.places.SearchBox(yInput, {bounds: worldBounds});
-
-  // get user's location
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showLocation);
-  } else {
-    alert("You need to enable geolocation!");
-    location.reload(true);
-  }
 }
