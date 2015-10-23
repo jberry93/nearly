23abd31a5ed9d9123e9777cd3e6bb05fd47c7a27
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-10-23 10:27:05 -0700

    Added working database

--- server/mongo.js
+++ server/mongo.js
@@ -3,16 +3,29 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/nearly");
 
 var database = mongoose.connection;
-database.on("error", console.log("connection error!"));
+database.on("error", console.error.bind(console, "connection error!"));
 database.once("open", function() {
   console.log("Connected!!");
 });
 
 var historySchema = mongoose.Schema({
   term: String,
-  location: String,
-  distance: Number,
-  list: String
+  location: String
 });
 
 var List = mongoose.model("List", historySchema);
+
+var history1 = new List({
+  term: "food",
+  location: "Torrance, CA"
+});
+
+history1.save();
+
+List.find(function(error, lists) {
+  if(error) {
+    return console.log(error);
+  } else {
+    console.log(lists);
+  }
+});
