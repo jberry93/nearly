7114c64a1ba58ce9629ce6bf5df181c3e4cb6890
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-10-23 11:27:43 -0700

    Added mongodb to server.js, successfully logs input into database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,34 @@
 var express = require("express"),
     server = express(),
+    mongoose = require("mongoose"),
+    parser = require("body-parser"),
+    parseJSON = parser.json(),
     path = require("path");
+
+mongoose.connect("mongodb://localhost/nearly");
 
 server.get("/", function(request, response) {
   response.sendFile(path.join(__dirname + "/public/index.min.html"));
+});
+
+server.get("/history", function(request, response) {
+  mongoose.model("history").find(function(error, history) {
+    response.send(history);
+  });
+});
+
+var historySchema = mongoose.Schema({
+  term: String,
+  location: String
+});
+
+var YourHistory = mongoose.model("history", historySchema);
+
+server.post("/", parseJSON, function(request, response) {
+  console.log(request.body);
+  var history = new YourHistory(request.body);
+  history.save();
+  response.send("Got it!");
 });
 
 server.use(express.static(__dirname + "/public"));
